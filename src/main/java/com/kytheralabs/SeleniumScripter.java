93ff2911f1074041f51b619d80b33695bf80e8fb
Tom Barber <tom@spicule.co.uk>
2021-03-15 14:48:17 +0000

    update test example

--- src/main/java/com/kytheralabs/SeleniumScripter.java
+++ src/main/java/com/kytheralabs/SeleniumScripter.java
@@ -15,33 +15,45 @@ import java.util.Map;
 public class SeleniumScripter {
     private WebDriver driver;
     private Map<String, Object> masterScript;
-    private Map<String, List<ArrayList>> captureLists = new HashMap<>();
+    private Map<String, List<String>> captureLists = new HashMap<>();
     private List<String> snapshots = new ArrayList<>();
+    private Integer iteration = 0;
+    private String loopValue;
 
-    public void runScript(Map<String, Object> script) throws Exception {
+    public SeleniumScripter(WebDriver webDriver){
+        driver = webDriver;
+    }
+    public void runScript(Map<String, Object> script, Integer iteration, String loopValue) throws Exception {
         System.out.println("Processing Selenium Script");
         System.out.println("Objects found: "+script.size());
-        masterScript = script;
+        this.iteration = iteration;
+        this.loopValue = loopValue;
+        if(masterScript == null){
+            masterScript = script;
+        }
         for (Map.Entry me : script.entrySet()) {
             System.out.println("Key: "+me.getKey() + " & Value: " + me.getValue());
             if(me.getValue() instanceof Map){
                 Map<String, Object> obj = (Map<String, Object>) me.getValue();
-                if(obj.get("operation").equals("select")){
-                    runSelect(obj);
-                } else if(obj.get("operation").equals("keys")){
-                    runKeys(obj);
-                } else if(obj.get("operation").equals("wait")){
-                    runWait(obj);
-                } else if(obj.get("operation").equals("captureList")){
-                    captureList(obj);
-                } else if(obj.get("operation").equals("loop")){
-                    loop(obj);
-                } else if(obj.get("operation").equals("click")){
-                    click(obj);
-                } else if(obj.get("operation").equals("clickListItem")){
-                    clickListItem(obj);
-                } else if(obj.get("operation").equals("snapshot")){
-                    snapshot();
+                System.out.println("Operation found: "+ obj.get("operation"));
+                if(obj.containsKey("operation")) {
+                    if (obj.get("operation").equals("select")) {
+                        runSelect(obj);
+                    } else if (obj.get("operation").equals("keys")) {
+                        runKeys(obj);
+                    } else if (obj.get("operation").equals("wait")) {
+                        runWait(obj);
+                    } else if (obj.get("operation").equals("captureList")) {
+                        captureList(obj);
+                    } else if (obj.get("operation").equals("loop")) {
+                        loop(obj);
+                    } else if (obj.get("operation").equals("click")) {
+                        click(obj);
+                    } else if (obj.get("operation").equals("clickListItem")) {
+                        clickListItem(obj);
+                    } else if (obj.get("operation").equals("snapshot")) {
+                        snapshot();
+                    }
                 }
             }
 
@@ -123,10 +135,13 @@ public class SeleniumScripter {
         WebElement element = selectElement(script.get("selector").toString(), script.get("name").toString());
         Select selectObj = new Select(element);
         if(script.get("selectBy").equals("value")){
+            System.out.println("Run select by value");
             selectObj.selectByValue(script.get("value").toString());
         } else if(script.get("selectBy").equals("index")){
+            System.out.println("Run select by index");
             selectObj.selectByIndex(Integer.parseInt(script.get("value").toString()));
         } else if(script.get("selectBy").equals("visible")){
+            System.out.println("Run select by visible text");
             selectObj.selectByVisibleText(script.get("value").toString());
         }
     }
@@ -134,7 +149,16 @@ public class SeleniumScripter {
     private void runKeys(Map<String, Object> script) throws Exception {
         WebElement element = selectElement(script.get("selector").toString(), script.get("name").toString());
         String keystring = script.get("value").toString();
-        element.sendKeys(keystring);
+        String target = keystring;
+        if(keystring.equals("${loopvalue}")){
+            target = this.loopValue;
+        }
+        element.clear();
+        for(char s : target.toCharArray()){
+            System.out.println("Inserting: "+String.valueOf(s));
+            element.sendKeys(String.valueOf(s));
+            Thread.sleep(300);
+        }
     }
 
     private void runWait(Map<String, Object> script) throws Exception {
@@ -143,35 +167,52 @@ public class SeleniumScripter {
             waittimeout = Integer.parseInt(script.get("timeout").toString());
         }
         WebDriverWait wait = new WebDriverWait(driver, waittimeout);
-
+        System.out.println("Waiting for object: "+script.get("name").toString());
         wait.until(ExpectedConditions.visibilityOfElementLocated(ByElement(script.get("selector").toString(), script.get("name").toString())));
-
+        System.out.println("Object found");
     }
 
-    private void captureList(Map<String, Object> script){
-
+    private void captureList(Map<String, Object> script) throws Exception {
+        System.out.println("Generating Capture List");
+        List<WebElement> webElements = selectElements(script.get("selector").toString(), script.get("name").toString());
+        List<String> strlist = new ArrayList<>();
+        for(WebElement el : webElements){
+            System.out.println("Capture Element Found: "+el.getText());
+            strlist.add(el.getText());
+        }
+        System.out.println("Storing capture list as: "+script.get("variable").toString());
+        captureLists.put(script.get("variable").toString(), strlist);
     }
 
     private void loop(Map<String, Object> script) throws Exception {
         String loopType = script.get("type").toString();
         if(loopType.equals("variable")){
-            Map<String, Object> subscripts = (Map<String, Object>) masterScript.get("subscripts");
-            Map<String, Object> subscript = (Map<String, Object>) subscripts.get(script.get("subscript"));
-            runScript(subscript);
+            List<String> vars = captureLists.get(script.get("variable").toString());
+            System.out.println("Performing Variable Loop for: "+script.get("variable").toString());
+            for (String v : vars) {
+                Map<String, Object> subscripts = (Map<String, Object>) masterScript.get("subscripts");
+                Map<String, Object> subscript = (Map<String, Object>) subscripts.get(script.get("subscript"));
+                System.out.println("Looping for variable: " + v+ " . Using subscript: "+ script.get("subscript"));
+                runScript(subscript, null, v);
+            }
         }
     }
 
     private void click(Map<String, Object> script) throws Exception {
         WebElement element = selectElement(script.get("selector").toString(), script.get("name").toString());
+        System.out.println("Clicking Element");
         element.click();
     }
 
     private void clickListItem(Map<String, Object> script) throws Exception {
         List<WebElement> element = selectElements(script.get("selector").toString(), script.get("name").toString());
-        element.get(Integer.parseInt(script.get("item").toString())).click();
+        int i = Integer.parseInt(script.get("item").toString());
+        System.out.println("Clicking list item");
+        element.get(i).click();
     }
 
     private void snapshot(){
+        System.out.println("Taking Snapshot");
         snapshots.add(driver.getPageSource());
     }
 
--- src/test/java/com/kytheralabs/TestSeleniumScripter.java
+++ src/test/java/com/kytheralabs/TestSeleniumScripter.java
@@ -1,9 +1,14 @@
 package com.kytheralabs;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Map;
 
 public class TestSeleniumScripter {
@@ -17,7 +22,20 @@ public class TestSeleniumScripter {
         Map<String, Object> obj = yaml.load(inputStream);
         System.out.println(obj);
 
-        SeleniumScripter s = new SeleniumScripter();
-        s.runScript(obj);
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        //capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+        String loc = "http://localhost:3000/webdriver";
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+
+
+        WebDriver webDriver = new RemoteWebDriver(new URL(loc), capabilities);
+
+        webDriver.get("https://www.upmchealthplan.com/find-a-medication/default.aspx#medication");
+        SeleniumScripter s = new SeleniumScripter(webDriver);
+        s.runScript(obj, null, null);
     }
 }
