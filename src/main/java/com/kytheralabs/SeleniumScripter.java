dac95bab32bd2c43d5820d04562260b595feb5be
Tom Barber <tom@spicule.co.uk>
2021-05-19 21:02:32 +0100

    fix Juan's dodgy pull request

--- src/main/java/com/kytheralabs/SeleniumScripter.java
+++ src/main/java/com/kytheralabs/SeleniumScripter.java
@@ -320,35 +320,36 @@ public class SeleniumScripter {
     private void runWait(Map<String, Object> script) throws Exception {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         int waittimeout = 30;
-        if(script.containsKey("timeout")){
+        if (script.containsKey("timeout")) {
             waittimeout = ((Double) script.get("timeout")).intValue();
             //waittimeout = (Integer) script.get("timeout");
         }
 
-        if(script.get("selector").toString().equals("none")){
-           // driver.manage().timeouts().implicitlyWait(waittimeout, SECONDS);
+        if (script.get("selector").toString().equals("none")) {
+            // driver.manage().timeouts().implicitlyWait(waittimeout, SECONDS);
         } else {
             System.out.println("Waiting for object: " + script.get("name").toString());
             //new WebDriverWait(driver, waittimeout)
 
 
-            if(waittimeout == 180){
+            if (waittimeout == 180) {
                 wait.until(ExpectedConditions.visibilityOfElementLocated(ByElement(script.get("selector").toString(), script.get("name").toString())));
-            } else{
+            } else {
                 waits.until(ExpectedConditions.visibilityOfElementLocated(ByElement(script.get("selector").toString(), script.get("name").toString())));
             }
 
             System.out.println("Object found");
-            
-        if (script.containsKey("asyncwait") && script.get("asyncwait").equals(true)) {
-            //To set the script timeout to 10 seconds
-            driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
-            //To declare and set the start time
-            long startTime = System.currentTimeMillis();
-            //Calling executeAsyncScript() method to wait for 10 seconds
-            js.executeAsyncScript("window.setTimeout(arguments[arguments.length - 1], 10000);");
-            //To get the difference current time and start time
-            System.out.println("Wait time: " + (System.currentTimeMillis() - startTime));
+
+            if (script.containsKey("asyncwait") && script.get("asyncwait").equals(true)) {
+                //To set the script timeout to 10 seconds
+                driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+                //To declare and set the start time
+                long startTime = System.currentTimeMillis();
+                //Calling executeAsyncScript() method to wait for 10 seconds
+                js.executeAsyncScript("window.setTimeout(arguments[arguments.length - 1], 10000);");
+                //To get the difference current time and start time
+                System.out.println("Wait time: " + (System.currentTimeMillis() - startTime));
+            }
         }
     }
     private void pause(Integer milliseconds){
