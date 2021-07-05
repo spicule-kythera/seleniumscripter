f651c2cafca4abe2c89b01fd41c731cf397e2257
Tom Barber <tom@spicule.co.uk>
2021-07-05 20:35:05 +0100

    remove errored code

--- src/main/java/com/kytheralabs/SeleniumScripter.java
+++ src/main/java/com/kytheralabs/SeleniumScripter.java
@@ -5,8 +5,6 @@ import groovy.lang.GroovyShell;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONException;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.openqa.selenium.NoSuchElementException;
